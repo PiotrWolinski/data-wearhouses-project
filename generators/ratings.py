5c7c51737bbf49de89dc679ea44baa8c83c3123c
mkuczynski11 <59289239+mkuczynski11@users.noreply.github.com>
2021-10-28 06:46:42 -0700

    fixed problem with call centers (#8)

--- generators/ratings.py
+++ generators/ratings.py
@@ -5,13 +5,10 @@ import pathlib
 class RatingGenerator:
 
     START_ID = 0
-    RATINGS_PER_SURVEY = (0,3)
-    RATING_TYPES = ["consultant rating", "connection rating", "solution rating"]
-    RATING_TYPES_WEIGHTS = [0.7, 0.1, 0.2]
+    RATING_TYPES = ["consultant rating", "solution rating"]
     RATINGS = [1, 2, 3, 4, 5]
     RATING_WEIGHTS = {
         "consultant rating": [0.05, 0.15, 0.4, 0.35, 0.05],
-        "connection rating": [0.15, 0.05, 0.1, 0.5, 0.2],
         "solution rating": [0.2, 0.1, 0.3, 0.2, 0.2]
     }
 
@@ -46,10 +43,8 @@ class RatingGenerator:
         for i in range(self._surveys_amount):
             current_id = self.current_id
             survey = i+1
-            ratings_amount = random.randint(*self.RATINGS_PER_SURVEY)
-            types = random.sample(self.RATING_TYPES, k=ratings_amount)
 
-            for type in types:
+            for type in self.RATING_TYPES:
                 rating_value = random.choices(self.RATINGS, self.RATING_WEIGHTS[type])[0]
                 self._ratings.append(self.Rating(id=current_id,
                                                 survey=survey,
